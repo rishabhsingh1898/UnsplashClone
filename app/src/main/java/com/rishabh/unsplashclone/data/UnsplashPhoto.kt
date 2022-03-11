2a19287e4b3705a4c9ecfbb425ec38f8bf0785cf
Rishabh Singh <ris90909@gmail.com>
2022-03-11 20:05:15 +0530

    created nav graph for navigation

--- app/src/main/java/com/rishabh/unsplashclone/data/UnsplashPhoto.kt
+++ app/src/main/java/com/rishabh/unsplashclone/data/UnsplashPhoto.kt
@@ -1,4 +1,4 @@
-package com.rishabh.unsplashclone
+package com.rishabh.unsplashclone.data
 
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
---
+++ app/src/main/java/com/rishabh/unsplashclone/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,8 @@
+package com.rishabh.unsplashclone.ui.gallery
+
+import androidx.fragment.app.Fragment
+import com.rishabh.unsplashclone.R
+
+class GalleryFragment : Fragment(R.layout.fragment_gallery) {
+
+}
