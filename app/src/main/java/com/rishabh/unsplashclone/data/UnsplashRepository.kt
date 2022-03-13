1871b4bbfb54e84882fa224cf84f29584a25328e
Rishabh Singh <ris90909@gmail.com>
2022-03-13 21:57:53 +0530

    added code for pagination

--- app/src/main/java/com/rishabh/unsplashclone/data/UnsplashRepository.kt
+++ app/src/main/java/com/rishabh/unsplashclone/data/UnsplashRepository.kt
@@ -1,9 +1,22 @@
 package com.rishabh.unsplashclone.data
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.liveData
 import com.rishabh.unsplashclone.api.UnsplashApi
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class UnsplashRepository @Inject constructor(private val unsplashApi: UnsplashApi) {
+    fun getSearchResult(query: String) =
+        Pager(
+            config = PagingConfig(
+                pageSize = 20,
+                maxSize = 100,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = { UnsplashPagingSource(unsplashApi, query) }
+        ).liveData
+
 }
--- app/src/main/java/com/rishabh/unsplashclone/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/rishabh/unsplashclone/ui/gallery/GalleryViewModel.kt
@@ -1,9 +1,29 @@
 package com.rishabh.unsplashclone.ui.gallery
 
+import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import androidx.paging.cachedIn
 import com.rishabh.unsplashclone.data.UnsplashRepository
 
-class GalleryViewModel @ViewModelInject constructor(private val repository: UnsplashRepository) :
+class GalleryViewModel @ViewModelInject constructor(
+    private val repository: UnsplashRepository,
+    @Assisted state: SavedStateHandle
+) :
     ViewModel() {
+
+    private val currentQuery = state.getLiveData(CURRENT_QUERY, DEFAULT_QUERY)
+
+    val photos = currentQuery.switchMap { it ->
+        repository.getSearchResult(it).cachedIn(viewModelScope)
+    }
+
+    fun searchPhotos(query: String) {
+        currentQuery.value = query
+    }
+
+    companion object {
+        private const val DEFAULT_QUERY = "cars"
+        private const val CURRENT_QUERY = "current_query"
+    }
 }
