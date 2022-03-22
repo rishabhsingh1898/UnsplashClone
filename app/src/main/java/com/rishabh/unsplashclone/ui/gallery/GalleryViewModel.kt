2c3188d12f539a4674c05e7428e37e23984109cb
Rishabh Singh <ris90909@gmail.com>
2022-03-22 22:33:46 +0530

    used hilt dagger to implement dependencies injection, injected activity, viewmodel, retrofit instances

--- app/src/main/java/com/rishabh/unsplashclone/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/rishabh/unsplashclone/ui/gallery/GalleryViewModel.kt
@@ -1,20 +1,21 @@
 package com.rishabh.unsplashclone.ui.gallery
 
-import androidx.hilt.Assisted
-import androidx.hilt.lifecycle.ViewModelInject
+
 import androidx.lifecycle.*
 import androidx.paging.cachedIn
 import com.rishabh.unsplashclone.data.UnsplashRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class GalleryViewModel @ViewModelInject constructor(
+@HiltViewModel
+class GalleryViewModel @Inject constructor(
     private val repository: UnsplashRepository,
-    @Assisted state: SavedStateHandle
 ) :
     ViewModel() {
 
-    private val currentQuery = state.getLiveData(CURRENT_QUERY, DEFAULT_QUERY)
+    private val currentQuery = MutableLiveData(DEFAULT_QUERY)
 
-    val photos = currentQuery.switchMap { it ->
+    val photos = currentQuery.switchMap {
         repository.getSearchResult(it).cachedIn(viewModelScope)
     }
 
