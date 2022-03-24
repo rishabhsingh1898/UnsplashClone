6374bed95a237021807613b28a166e49ba88e0b1
Rishabh Singh <ris90909@gmail.com>
2022-03-24 21:01:42 +0530

    added detail fragment and added interface to implement click listener on recyclerview

---
+++ app/src/main/java/com/rishabh/unsplashclone/ui/details/DetailsFragment.kt
@@ -0,0 +1,7 @@
+package com.rishabh.unsplashclone.ui.details
+
+import androidx.fragment.app.Fragment
+import com.rishabh.unsplashclone.R
+
+class DetailsFragment : Fragment(R.layout.fragment_details) {
+}
